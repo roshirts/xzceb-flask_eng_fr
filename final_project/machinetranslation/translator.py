917a7369f27447338542c626b89ab82818cca3f8
roshirts <134865905+roshirts@users.noreply.github.com>
2023-05-28 22:28:36 +0300

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,1 +1,44 @@
+import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
 
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text : str):
+    #gets string in englsh // returns string in french
+    try:
+        translation = language_translator.translate(
+            text=english_text,
+            model_id='en-fr').get_result()
+        #Getting the pure translation string
+        french_text = translation['translations'][0]['translation']
+        return french_text
+    except Exception as error:
+        print(error)
+        return None
+
+def french_to_english(french_text : str):
+    #gets string in french // returns string in english
+    try:
+        translation = language_translator.translate(
+            text=french_text,
+            model_id='fr-en').get_result()
+        #Getting the pure translation string
+        english_text = translation['translations'][0]['translation']
+        return english_text
+    except Exception as error:
+        print(error)
+        return None
