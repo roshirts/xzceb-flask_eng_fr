3a1b73fd6c841045727668ed8ae7c51bbcd99a78
roshirts <roshirts@gmail.com>
2023-05-29 12:32:54 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -13,12 +13,12 @@ def englishToFrench():
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    english_text = translator.english_to_french(textToTranslate)
+    english_text = translator.french_to_english(textToTranslate)
     return english_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
